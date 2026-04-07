14cf2a55f54a484962b3f3e37a203e1afa3b13da
Katie-Schwerer <katieschwerer@gmail.com>
2026-04-06 21:13:57 -0400

    Trying to figure login and signup logic

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:ABqBK4F0sAKhXMpuc0ZBPB2h@172.21.213.219:27017";
+const mongoURI =  "mongodb://root:55lKbWi4khsRoslAnPc8xzoG@172.21.136.41:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
