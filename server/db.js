737a418e52130fe681fc6c7384f20075e874b685
Katie-Schwerer <katieschwerer@gmail.com>
2026-04-11 10:32:04 -0400

    Getting close to finishing you the Review Component

--- server/db.js
+++ server/db.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-const mongoURI =  "mongodb://root:igyLKesROF3UKU6E2A8jxDV7@172.21.56.127:27017";
+const mongoURI =  "mongodb://root:GUEzmbxhHKpQVWfiT84lUzwq@172.21.74.177:27017";
 
 const connectToMongo = async (retryCount) => {
     const MAX_RETRIES = 3;
--- src/Components/ReviewForm/ReviewForm.jsx
+++ src/Components/ReviewForm/ReviewForm.jsx
@@ -1,14 +1,27 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './ReviewForm.css'
+import GiveReviews from '../GiveReviews/GiveReviews';
 
 let doctor = [{ 'serial_number': 1, 'name': 'Dr. John Doe', 'speciality': 'Cardiology' }, { 'serial_number': 2, 'name': 'Dr. Jane Smith', 'speciality': 'Dermatology' }]
 
 function ReviewForm() {
+    const [showModal, setShowModal] = useState(false);
+    const [isButtonDisabled, setIsButtonDisabled] = useState(false);
+
+    const handleOpenModal = () => {
+        setShowModal(true);
+    };
+
+    const handleReviewComplete = () => {
+        setShowModal(false);
+        setIsButtonDisabled(true);
+    };
+
     return (
-        <div class="review-component">
-            <div class="review-content">
+        <div className="review-component">
+            <div className="review-content">
                 <h1>Reviews</h1>
-                <table class="review-table">
+                <table className="review-table">
                     <tr>
                         <th>Serial Number</th>
                         <th>Doctor Name</th>
@@ -16,15 +29,24 @@ function ReviewForm() {
                         <th>Provide Feedback</th>
                         <th>Review Given</th>
                     </tr>
-                    {doctor.map(doc => <tr>
+                    {doctor.map(doc => <tr key={doc.serial_number}>
                         <td>{doc.serial_number}</td>
                         <td>{doc.name}</td>
                         <td>{doc.speciality}</td>
-                        <td><button class="review-button">Click Here</button></td>
+                        <td>
+                            <button onClick={handleOpenModal} disabled={isButtonDisabled}>
+                                Click Here
+                            </button>
+                        </td>
                         <td></td>
                     </tr>)}
                 </table>
             </div>
+            {showModal && (
+                    <div className="modal">
+                        <GiveReviews onComplete={handleReviewComplete} />
+                    </div>
+                )}
         </div>
     );
 }
